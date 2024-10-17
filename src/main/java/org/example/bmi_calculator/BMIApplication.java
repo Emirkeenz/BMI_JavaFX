4746b380a00444dd0bbdfcbb14a0996ace90e76c
Emirkeenz <orozbekovjob@gmail.com>
2024-10-17 23:36:20 +0600

    made responsive fxml

--- src/main/java/org/example/bmi_calculator/BMIApplication.java
+++ src/main/java/org/example/bmi_calculator/BMIApplication.java
@@ -11,7 +11,7 @@ public class BMIApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BMIApplication.class.getResource("bmi.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("JavaFX BMI Calculator!");
         stage.setScene(scene);
         stage.show();
